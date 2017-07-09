3412a92a1753437b5a2d50198cd2105a6134b451
Blake Estes <blakee.estes90@gmail.com>
2017-07-09 12:56:08 -0500

    Lesson 8 Completion

--- callbacks.js
+++ callbacks.js
@@ -17,6 +17,9 @@
 
 
 // Write a function called firstItem that passes the first item of the given array to the callback function
+function firstItem(arr, cb) {
+  cb(arr[0]);
+}
 
 var foods = ['pineapple', 'mango', 'ribeye', 'curry', 'tacos', 'ribeye', 'mango'];
 
@@ -25,24 +28,36 @@ firstItem(foods, function(firstItem) {
 });
 
 // Write a function called getLength that passes the length of the array into the callback
+function getLength(arr, cb) {
+  cb(arr.length);
+}
 
 getLength(foods, function(length) {
   console.log('The length of the array is ' + length);
 });
 
 // Write a function called last which passes the last item of the array into the callback
+function last(arr, cb) {
+	cb(arr[arr.length -1]);
+}
 
 last(foods, function(lastItem) {
   console.log('The last item in the array is ' + lastItem);
 });
 
 // Write a function called sumNums that adds two numbers and passes the result to the callback
+function sumNums(num1, num2, cb) {
+  cb(num1 + num2);
+}
 
 sumNums(5, 10, function(sum) {
   console.log('The sum is ' + sum);
 });
 
-// Write a function called multiplyNums that adds two numbers and passes the result to the callback
+// Write a function called multiplyNums that multiplies two numbers and passes the result to the callback
+function multiplyNums(num1, num2, cb) {
+  cb(num1 * num2);
+}
 
 multiplyNums(5, 10, function(product) {
   console.log('The product is ' + product);
@@ -50,6 +65,13 @@ multiplyNums(5, 10, function(product) {
 
 // Write a function called contains that checks if an item is present inside of the given array.
 // Pass true to the callback if it is, otherwise pass false
+function contains(arr, product, cb) {
+  if(arr.includes(product)) {
+    cb(true);
+  } else {
+    cb(false);
+  }
+}
 
 contains(foods, 'ribeye', function(result) {
   console.log(result ? 'ribeye is in the array' : 'ribeye is not in the array');
@@ -58,11 +80,24 @@ contains(foods, 'ribeye', function(result) {
 // Write a function called removeDuplicates that removes all duplicate values from the given array.
 // Pass the array to the callback function.  Do not mutate the original array.
 
+function removeDuplicates(arr, cb){
+ var duplicatesRemoved=[];
+ for(i = 0; i < arr.length; i++) {
+	if(duplicatesRemoved.indexOf(arr[i])==-1) duplicatesRemoved.push(arr[i]);
+ }
+  cb(duplicatesRemoved);
+}
+
 removeDuplicates(foods, function(uniqueFoods) {
   console.log('foods with duplicates removed: ' + uniqueFoods);
 });
 
 // Write a function called forEach that iterates over the provided array and passes the value and index into the callback.
+function forEach(arr, cb) {
+	for(i = 0; i < arr.length; i++) {
+		cb(arr[i], index=i);
+	}
+}
 
 forEach(foods, function(value, index) {
   console.log(value + ' is at index ' + index);
